3deb8d67f7921a2436eac85ed0572b981d471bbb
samdjames <sam.d.james@gmail.com>
2017-03-12 19:44:43 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m_inv <- NULL
+        set <- function(y) {
+                x <<- y
+                m_inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m_inv <<- inverse
+        getinverse <- function() m_inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+        m_inv <- x$getinverse()
+        if(!is.null(m_inv)) {
+                message("getting cached data of the ineverse of matrix")
+                return(m_inv)
+        }
+        matrix <- x$get()
+        m_inv <- solve(matrix, ...)
+        x$setinverse(m_inv)
+        m_inv
 }
+## Pass the value that is returned from the makeCacheMatrix function rather than the function itself 
+## which will throw an error for atomic vector
+cachevalue<-numeric()
+cachevalue<-makeCacheMatrix(a)
+cachesolve(cachevalue)
+
+
